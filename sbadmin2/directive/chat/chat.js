d3e72a6c5eccd84c3186e2024b7367016ca85022
Sungwoo <swcho@humaxdigital.com>
2016-01-08 09:17:55 +0900

    Enable All Directive Example In Dashboard Partial

--- sbadmin2/directive/chat/chat.js
+++ sbadmin2/directive/chat/chat.js
@@ -11,7 +11,7 @@ angular.module('sbadmin2').directive('chat', function (SBADMIN2_ROOT) {
         restrict: 'E',
         replace: true,
         scope: {
-            title: '='
+            title: '@'
         }
     };
 });
